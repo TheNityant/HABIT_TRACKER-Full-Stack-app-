e9f8f9423682791c8920a754341c1285ab0ff450
TheNityant <nityant.tiwari@nldamia.co.in>
2026-02-14 09:39:22 +0530

    Updating user java for login page

--- Habit tracker/habit_tracker backend/tracker/tracker/src/main/java/com/habit/tracker/model/User.java
+++ Habit tracker/habit_tracker backend/tracker/tracker/src/main/java/com/habit/tracker/model/User.java
@@ -14,10 +14,10 @@ public class User {
     @Column(name = "user_id") // MAPPING: Java 'id' = SQL 'user_id' (The real PK)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(name= "username", unique = true)
     private String username; // SQL: username
 
-    @Column(nullable = false, unique = true)
+    @Column(name= "email", unique = true)
     private String email;    // SQL: email
 
     @Column(nullable = false)
@@ -33,4 +33,20 @@ public class User {
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
+
+    // --- GETTERS AND SETTERS ---
+    // (You can generate these in IntelliJ/VS Code: Right Click -> Generate -> Getters and Setters)
+
+    public Long getuser_id() { return id;}
+    public void setuser_id(Long user_id) { this.id = user_id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    
+    public int getXpScore() { return xpScore; }
+    public void setXpScore(int xpScore) { this.xpScore = xpScore; }
+
 }
