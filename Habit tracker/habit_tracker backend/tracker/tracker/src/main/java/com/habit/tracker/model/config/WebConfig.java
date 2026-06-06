1d75649e8d91bf519a02af85e3ff248d43c17b17
Nityant Tiwari <nityant.tiwari@nldamia.co.in>
2026-06-06 16:54:02 -0400

    Adding AI JOURNLING logic

--- Habit tracker/habit_tracker backend/tracker/tracker/src/main/java/com/habit/tracker/model/config/WebConfig.java
+++ Habit tracker/habit_tracker backend/tracker/tracker/src/main/java/com/habit/tracker/model/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.habit.tracker.config;
+package com.habit.tracker.model.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
